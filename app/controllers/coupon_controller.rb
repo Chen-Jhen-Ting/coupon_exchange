2df3d48bc2d822814b7ebcf175884d4aea4056cf
Chen-Jhen-Ting <67670844+Chen-Jhen-Ting@users.noreply.github.com>
2020-11-02 21:57:35 +0800

    Revert " coupon controller create action"
    This reverts commit d18ecba8f2a6c1c857a3c6b9e5de1ff96e90672e.

--- app/controllers/coupon_controller.rb
+++ app/controllers/coupon_controller.rb
@@ -4,20 +4,6 @@ class CouponController < ApplicationController
   end
 
   def create
-    @coupon = current_user.build_coupon(coupon_params)
     
-    if @coupon.save
-      redirect_to root_path, notice: "已成功兌換，您的兌換卷序號為 #{ @coupon.uuid }"
-    else
-      render :new
-    end
-
-  end
-
-  private
-  def coupon_params
-    params.require(:coupon)
-          .permit(:name, :twid, :phone)
-          .merge(user: current_user)
   end
 end
