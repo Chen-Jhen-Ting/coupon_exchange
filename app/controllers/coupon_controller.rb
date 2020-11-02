219bd7dac657e52ce159121a7f775bd4cf6b798a
Chen-Jhen-Ting <67670844+Chen-Jhen-Ting@users.noreply.github.com>
2020-11-02 21:57:35 +0800

    Revert "coupon controller - authenticate_user! - check whether got coupon - show when you got coupon at notice"
    This reverts commit 76d3a8b3a79370060e819e870bd53f70a05f2591.

--- app/controllers/coupon_controller.rb
+++ app/controllers/coupon_controller.rb
@@ -1,5 +1,4 @@
 class CouponController < ApplicationController
-  before_action :authenticate_user!
   def new
     @coupon = Coupon.new
   end
@@ -7,9 +6,7 @@ class CouponController < ApplicationController
   def create
     @coupon = Coupon.new(coupon_params)
     
-    if got_coupon
-      show_when_you_got
-    elsif @coupon.save
+    if @coupon.save
       redirect_to root_path, notice: "已成功兌換，您的兌換卷序號為 #{ @coupon.uuid }"
     else
       render :new
@@ -23,16 +20,4 @@ class CouponController < ApplicationController
           .permit(:name, :twid, :phone)
           .merge(user: current_user)
   end
-
-  def got_coupon
-    got_coupon = User.find(@coupon.user_id).coupon
-  end
-
-  def show_when_you_got
-    create_time = "#{got_coupon.created_at}"
-    create_time = create_time.split(" ")[0].split("-")
-    flash[:notice] = "您已於 #{create_time[0]} 年 #{create_time[1]} 月 #{create_time[2]} 日兌換過"
-    render :new 
-  end
-
 end
--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -3,10 +3,10 @@ class Coupon < ApplicationRecord
 
   validates :name , presence: {message: '姓名不可為空' }
   validates :phone, presence: {message: '手機號碼不可為空' }
-  validates :twid, taiwanese_id: {case_sensitive: false ,message: "您的身分證字號有誤，請確認後重新輸入"}
-  # validates :user_id, uniqueness: { message: "您已於兌換過#{Coupon.find_by(user_id: @coupon.user_id).create_at}" }
+  validates :twid, taiwanese_id: {message: "您的身分證字號有誤，請確認後重新輸入"}
+
   before_create :create_uuid
- 
+  
   private
   def create_uuid
     self.uuid = serial_generator(10)
