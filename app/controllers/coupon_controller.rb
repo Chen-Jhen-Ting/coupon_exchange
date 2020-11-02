bb594caab06d8945d73ee7bed39f2a2e50f9eabb
Ted Chen <teddy70642000@gmail.com>
2020-11-02 21:51:57 +0800

    大大的幫助！

--- app/controllers/coupon_controller.rb
+++ app/controllers/coupon_controller.rb
@@ -6,10 +6,7 @@ class CouponController < ApplicationController
 
   def create
     @coupon = Coupon.new(coupon_params)
-    
-    if got_coupon
-      show_when_you_got
-    elsif @coupon.save
+    if @coupon.save
       redirect_to root_path, notice: "已成功兌換，您的兌換卷序號為 #{ @coupon.uuid }"
     else
       render :new
@@ -22,17 +19,6 @@ class CouponController < ApplicationController
     params.require(:coupon)
           .permit(:name, :twid, :phone)
           .merge(user: current_user)
-  end
-
-  def got_coupon
-    got_coupon = User.find(@coupon.user_id).coupon
-  end
-
-  def show_when_you_got
-    create_time = "#{got_coupon.created_at}"
-    create_time = create_time.split(" ")[0].split("-")
-    flash[:notice] = "您已於 #{create_time[0]} 年 #{create_time[1]} 月 #{create_time[2]} 日兌換過"
-    render :new 
   end
 
 end
--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -4,7 +4,7 @@ class Coupon < ApplicationRecord
   validates :name , presence: {message: '姓名不可為空' }
   validates :phone, presence: {message: '手機號碼不可為空' }
   validates :twid, taiwanese_id: {case_sensitive: false ,message: "您的身分證字號有誤，請確認後重新輸入"}
-  # validates :user_id, uniqueness: { message: "您已於兌換過#{Coupon.find_by(user_id: @coupon.user_id).create_at}" }
+  validate :check_uniq_user
   before_create :create_uuid
  
   private
@@ -14,5 +14,12 @@ class Coupon < ApplicationRecord
 
   def serial_generator(n)
     [*'a'..'z', *'A'..'Z', *0..9].sample(n).join
+  end
+
+  def check_uniq_user
+    coupon = Coupon.find_by(user_id: user_id)
+    if coupon
+      errors.add(:uniq_user, "您已於兌換過#{coupon.created_at.strftime('%Y年 %m月 %d日 %T')}")
+    end
   end
 end
