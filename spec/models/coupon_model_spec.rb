3723c0070bcc89c2d3770f7ddc840e97209072d5
Ted Chen <teddy70642000@gmail.com>
2020-11-03 15:18:12 +0800

    rspec coupon uuid

--- spec/models/coupon_model_spec.rb
+++ spec/models/coupon_model_spec.rb
@@ -65,32 +65,47 @@ RSpec.describe Coupon, type: :model do
     end
   end
 
-  it 'can be created' do
-    user = User.create(
-      email: Faker::Internet.email,
-      password: Faker::String.random
-    )
-    coupon = user.create_coupon(
-      name: Faker::Name.name  ,
-      phone: Faker::PhoneNumber.cell_phone,
-      twid: TwidGenerator.generate
-    )
-    expect(coupon).to eq(Coupon.last)
+  describe ' Coupon create' do 
+    let(:user) do
+      User.create(
+        email: Faker::Internet.email,
+        password: Faker::String.random
+      )
+    end
+    
+    context 'with name, phone, twid' do
+      it 'should be ok' do
+        coupon = user.create_coupon(
+          name: Faker::Name.name  ,
+          phone: Faker::PhoneNumber.cell_phone,
+          twid: TwidGenerator.generate
+        )
+        expect(coupon).to eq(Coupon.last)
+      end
+    end
   end
 
-  it 'can be created with uuid' do
-    user = User.create(
-      email: Faker::Internet.email,
-      password: Faker::String.random
-    )
-    coupon = user.create_coupon(
-      name: Faker::Name.name  ,
-      phone: Faker::PhoneNumber.cell_phone,
-      twid: TwidGenerator.generate
-    )
-    expect(coupon.uuid).not_to eq("")
-    expect(coupon.uuid).to be_present
+  describe 'Coupon uuid generator' do
+    let(:user) do
+      User.create(
+        email: Faker::Internet.email,
+        password: Faker::String.random
+      )
+    end
+
+    context 'when user creates a coupon' do
+      it 'should be exist' do
+        coupon = user.create_coupon(
+          name: Faker::Name.name  ,
+          phone: Faker::PhoneNumber.cell_phone,
+          twid: TwidGenerator.generate
+        )
+        expect(coupon.uuid).not_to eq("")
+        expect(coupon.uuid).to be_present
+      end
+    end
   end
+
 
   it 'has id, name, phone, twid, uuid' do
     columns = Coupon.column_names
